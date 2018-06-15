96c2ca309a8cfa3a1f70dc007248091c650526ca
Xiu, Zhu <zhu.xiu@ge.com>
2018-06-15 23:43:56 +0800

    message box (UI complete)

--- src/js/containers/Invite/index.tsx
+++ src/js/containers/Invite/index.tsx
@@ -3,6 +3,7 @@ import { translate } from 'react-i18next'
 import { connect } from 'dva'
 import { flow } from 'noru-utils/lib'
 import Modal from '../../components/Modal'
+import classname from 'classname'
 import './index.scss'
 
 function Header({ t }) {
@@ -30,12 +31,6 @@ export class Login extends React.Component<any, State> {
     showModal: false,
   }
 
-  invite = () => {
-    // let { dispatch } = this.props
-    // dispatch({ type: 'invite/get/invite-response'})
-    console.log('invited')
-  }
-
   openModal = () => {
     this.setState({ showModal: true })
   }
@@ -43,8 +38,20 @@ export class Login extends React.Component<any, State> {
   closeModal = () => {
     this.setState({ showModal: false })
   }
+
+  invite = () => {
+    let { dispatch } = this.props
+    dispatch({ type: 'invite/get/invite-response'})
+  }
+
+  done = () => {
+    let { dispatch } = this.props
+    dispatch({ type: 'invite/set/invite-response'})
+    this.closeModal()
+  }
+
   render() {
-    let { t } = this.props // it's for i18n, for simplicity I cut corner.
+    let { t, inviteResponse, loading } = this.props // 't' is for i18n, for simplicity I cut corner.
     let { showModal } = this.state
     return (
       <div className="page-invite">
@@ -58,11 +65,34 @@ export class Login extends React.Component<any, State> {
         <Modal show={showModal} onClose={this.closeModal}>
           <div className="invite-form-wrapper">
             <div className="invite-form">
-              <div className="form-title">{t`Request an invite`}</div>
-              <input className="input" type="text" placeholder={t`Full name`} />
-              <input className="input" type="text" placeholder={t`Email`} />
-              <input className="input" type="text" placeholder={t`Confirm Email`} />
-              <a className="button is-primary" onClick={this.invite}>{t`Send`}</a>
+              { inviteResponse
+                ? <>
+                  <div className="form-title">{'errocode' ? t`Ooops...` : t`All done`}</div>
+                  <div>{'errorcode'
+                    ? t`error message`
+                    : t`You will be one of the first to experience Broccoli & Co. when we launch.`
+                  }
+                  </div>
+                  <a
+                    className={classname('button', loading && 'is-loading')}
+                    onClick={this.done}
+                  >
+                    {t`Ok`}
+                  </a>
+                </>
+                : <>
+                  <div className="form-title">{t`Request an invite`}</div>
+                  <input className="input" type="text" placeholder={t`Full name`} />
+                  <input className="input" type="text" placeholder={t`Email`} />
+                  <input className="input" type="text" placeholder={t`Confirm Email`} />
+                  <a
+                    className={classname('button is-primary', loading && 'is-loading')}
+                    onClick={this.invite}
+                  >
+                    {t`Send`}
+                  </a>
+                </>
+              }
             </div>
           </div>
         </Modal>
@@ -70,9 +100,14 @@ export class Login extends React.Component<any, State> {
     )
   }
 }
-
+function mapState2Props({ invite, loading }) {
+  return {
+    inviteResponse: invite.inviteResponse,
+    loading: loading.effects['invite/get/invite-response'],
+  }
+}
 export default flow(
   Login,
   translate(),
-  connect(({ invite: { inviteResponse }}) => ({ inviteResponse })),
+  connect(mapState2Props),
 )
